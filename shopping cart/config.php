b2944cd3c2dd5ea32ecd7c0d72d9f7467fbdb87a
Harsh Rawat <115210435+harshrawat777@users.noreply.github.com>
2024-04-28 17:28:59 +0530

    Add files via upload

---
+++ shopping cart/config.php
@@ -0,0 +1,12 @@
+<?php
+
+$conn = mysqli_connect('localhost','root','','shop_db');
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+} else {
+    echo "Connected successfully";
+}
+
+?>
